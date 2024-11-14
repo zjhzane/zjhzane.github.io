c449e8458351d0944070874a5d078028b9241fd2
zjhzane <zjhzane@163.com>
2024-11-14 16:09:04 +0800

    Site updated: 2024-11-14 16:09:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/20/入库说明/","2024/10/20/系统录入/","2024/10/31/地点查询/","2024/10/22/常用软件下载/","2024/10/20/hello-world/"];function toRandomPost(){
+var posts=["2024/10/20/系统录入/","2024/10/20/入库说明/","2024/11/14/应知应会/","2024/10/31/地点查询/","2024/10/22/常用软件下载/","2024/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
